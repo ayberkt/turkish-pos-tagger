7cce4557b8ea10769eb52851f6c854023a17fdbb
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-11-13 23:01:27 -0600

    Add ADT for representing a POS

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 data POS = Noun
+         | Adj
+         | Adv
          | Verb
+         | Pron
+         | Conj
+         | Det
+         | Postp
+         | Ques
+         | Interj
+         | Num
+         | Dup
+         | Punc
+         deriving (Eq, Show,)
 
 main = putStrLn "Hi"
