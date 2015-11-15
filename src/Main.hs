3c237ae52b89231c42fe4219963de2fcf5b37f68
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-11-15 01:30:55 -0600

    Preliminary XML parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE Arrows #-}
+{-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
 
 module Main where
 
-import Data.HMM
-import Control.Monad
-import System.IO
+-- import Data.HMM
+import System.IO hiding (putStr)
 import System.Directory
-import qualified Data.Vector.Storable as VS
-import qualified Text.XML.HXT.Core as HXT
+-- import qualified Data.Vector.Storable as VS
+import Text.XML.HXT.Core
+import Prelude hiding (words, putStr)
 
 data POS = Noun
          | Adj
@@ -24,7 +24,20 @@ data POS = Noun
          | Punc
          deriving (Eq, Show)
 
+parseFile :: FilePath -> IOStateArrow a b XmlTree
+parseFile f = readDocument [ withValidate no
+                           , withRemoveWS yes
+                           ] f
+
+words = deep (isElem >>> hasName "S") >>>
+  proc x -> do
+    word <- getText <<< getChildren <<< deep (hasName "W") -< x
+    returnA -< word
+
+
 main :: IO ()
 main = do
-  files <- getDirectoryContents "tb_corrected"
-  print $ files !! 2
+  files <- getDirectoryContents "tb_uni"
+  let file = "tb_uni/" ++ files !! 2
+  ws <- runX (readDocument [withValidate no] file >>> words)
+  putStrLn $ unwords ws
