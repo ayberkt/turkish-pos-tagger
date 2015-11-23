1251c7aeb03bd8b9a931e1af3e6d9aa2fc8dad7a
Ayberk Tosun <ayberk.tosun@gmail.com>
2015-11-22 21:38:04 -0600

    Use loadHMM to avoid model creation time

--- tag/Main.hs
+++ tag/Main.hs
@@ -1,18 +1,21 @@
 {-# LANGUAGE BangPatterns #-}
 
 module Main where
+
+import Prelude
 import Data.HMM (loadHMM, viterbi, HMM(..))
 import System.IO
 import Data.Array
+import Parse hiding (words)
 
 main :: IO ()
 main = do
-  trainedHMM <- (loadHMM "model.hmm") :: IO (HMM Int String)
+  trainedHMM <- (loadHMM "model.hmm") :: IO (HMM POS String)
   putStr "> "
   hFlush stdout
   input <- getLine
   let ws       = words input
       !wsArray = listArray (0, (length ws)-1) ws
-      !out      = viterbi trainedHMM wsArray
-  print $ head out
+      !out     = viterbi trainedHMM wsArray
+  print out
   return ()
